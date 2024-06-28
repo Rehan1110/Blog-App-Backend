7e629d70b77c38a45dad9c80719b736ee773b6a1
Rehan Abbas <abbasrehan054@gmail.com>
2024-06-28 23:56:17 +0530

    Backend completed

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes";
+import router from "./routes/user-routes";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+mongoose.set("strictQuery",false);
+
+const MONGO_URL = process.env.MONGO_URI
+const PORT = 5000
+
+mongoose.connect(MONGO_URL)
+        .then(() => {
+          console.log("Database connected successfully");
+          app.listen(PORT, () => {
+            console.log(`App is listening on PORT ${PORT}`);
+          });
+        })
+        .catch((err) => console.log(err));
